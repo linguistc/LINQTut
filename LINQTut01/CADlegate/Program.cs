b4ac05fe8bf7fa737eb20215bc770f8489625928
ahmedsameh <ahmadsameh1@proton.me>
2024-07-15 16:34:17 +0300

    Adding Tut01.

---
+++ LINQTut01/CADlegate/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CADelegate
+{
+    // delegate void MDelegate();
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method2(Method1);
+
+            Action action = Method1;
+
+            Method2(action);
+
+            Console.ReadKey();
+        }
+
+
+        static void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        static void Method2(Action action)
+        {
+            action();
+
+            Console.WriteLine("Method2");
+        }
+    }
+}
---
+++ LINQTut01/FunctionalProgramming/ExtensionFunctional01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public static class ExtensionFunctional01
+    {
+        public static IEnumerable<Employee> Filter(IEnumerable<Employee> source, Func<Employee, bool> predicate)
+        {
+            foreach (var e in source) 
+            {
+                if(predicate(e)) yield return e;
+            }
+        }
+
+        public static void Print<T>(IEnumerable<T> source, string title)
+        {
+            if (source == null)
+                return;
+            Console.WriteLine();
+            Console.WriteLine("┌───────────────────────────────────────────────────────┐");
+            Console.WriteLine($"│   {title.PadRight(52, ' ')}│");
+            Console.WriteLine("└───────────────────────────────────────────────────────┘");
+            Console.WriteLine();
+            foreach (var item in source)
+                Console.WriteLine(item);
+        }
+    }
+}
