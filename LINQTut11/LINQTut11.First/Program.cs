f3b596bc5724caa5b9144f4dde8571188d0b1e97
ahmedsameh <ahmadsameh1@proton.me>
2024-07-26 17:19:24 +0300

    Adding Tut11 [ElementAt, First, Last and Single].

---
+++ LINQTut11/LINQTut11.First/Program.cs
@@ -0,0 +1,13 @@
+﻿using Shared;
+
+var questions = QuestionBank.All;
+var firstQuestion = questions.First();
+Console.WriteLine(firstQuestion);
+
+//var someQuestion = questions.First(x => x.Title.Length == 0); //InvalidOperationException
+var someOtherQuestion = questions.FirstOrDefault(x => x.Title.Length == 0); //null
+
+if (someOtherQuestion is null)
+{
+    Console.WriteLine("Question is null");
+}
---
+++ LINQTut11/LINQTut11.Last/Program.cs
@@ -0,0 +1,14 @@
+﻿using Shared;
+
+var questions = QuestionBank.All;
+var lastQuestion = questions.Last();
+Console.WriteLine(lastQuestion);
+
+//var someQuestion = questions.Last(x => x.Title.Length == 0); //InvalidOperationException
+
+var someOtherQuestion = questions.LastOrDefault(x => x.Title.Length == 0); //null
+
+if (someOtherQuestion is null)
+{
+    Console.WriteLine("Question is null");
+}
