13ff199f8997607d92bd68787daf930fcb519e1b
ahmedsameh <ahmadsameh1@proton.me>
2024-07-26 18:22:46 +0300

    Adding Tut13 [Concatenation].

---
+++ LINQTut13/LINQTut13.Concatenation/Program.cs
@@ -0,0 +1,53 @@
+﻿using Shared;
+
+//RunMethod01();
+//RunMethod02();
+RunMethod03();
+RunMethod04();
+
+static void RunMethod01()
+{
+    var quiz1 = QuestionBank.Randomize(3);
+    var quiz2 = QuestionBank.Randomize(2);
+
+    var quiz3 = quiz1.Concat(quiz2);
+    quiz3.ToQuiz();
+}
+
+static void RunMethod02()
+{
+    var quiz1 = QuestionBank.Randomize(3);
+    var quiz2 = QuestionBank.Randomize(2);
+
+    var questionTitles = quiz1.Select(q => q.Title)
+        .Concat(quiz2.Select(x => x.Title));
+
+    foreach (var title in questionTitles)
+        Console.WriteLine(title);
+}
+
+static void RunMethod03()
+{
+    var quiz1 = QuestionBank.Randomize(3);
+    var quiz2 = QuestionBank.Randomize(2);
+
+    var questionTitles =
+        QuestionBank.Randomize(3).Select(q => q.Title)
+        .Concat(QuestionBank.Randomize(2).Select(x => x.Title))
+        .Concat(QuestionBank.GetQuestionRange(Enumerable.Range(11, 14)).Select(y => y.Title));
+    
+    foreach (var title in questionTitles)
+        Console.WriteLine(title);
+}
+
+static void RunMethod04()
+{
+    var quiz1 = QuestionBank.Randomize(3);
+    var quiz2 = QuestionBank.Randomize(2);
+
+    var quiz3 = new[] { quiz1, quiz2 }.SelectMany(quiz => quiz);
+
+    quiz3.ToQuiz();
+
+    
+}
---
+++ LINQTut13/Shared/Choice.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared
+{
+    public class Choice
+    {
+        public int Order { get; set; }
+        public string Description { get; set; }
+
+        
+    }
+}
