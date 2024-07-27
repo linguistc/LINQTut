e775c18edab5c42c9b82dc8c898abad3dcc2a4dc
ahmedsameh <ahmadsameh1@proton.me>
2024-07-27 16:18:32 +0300

    Adding Tut16 [Expression Tree].

---
+++ LINQTut16/LINQTut16.ExpressionTree01/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+Func<int, bool> IsEven = (num) => num % 2 == 0;
+Console.WriteLine(IsEven(10));
+Console.WriteLine(IsEven.Invoke(10));
+
+
+Expression<Func<int, bool>> IsEvenExpression = (num) => num % 2 == 0;
+Func<int, bool> IsEvenV2 = IsEvenExpression.Compile();
+Console.WriteLine(IsEvenV2(10));
---
+++ LINQTut16/LINQTut16.ExpressionTree03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+
+// (num) => num % 2 == 0
+
+ParameterExpression numParam = Expression.Parameter(typeof(int), "num");
+
+ConstantExpression zeroParam = Expression.Constant(0, typeof(int));
+ConstantExpression twoParam = Expression.Constant(2, typeof(int));
+BinaryExpression moduloBinaryExpression = Expression.Modulo(numParam, twoParam);
+BinaryExpression isEvenBinaryExpression = Expression.Equal(moduloBinaryExpression, zeroParam);
+
+Expression<Func<int, bool>> IsEvenExpression = Expression.Lambda<Func<int, bool>>(
+    isEvenBinaryExpression, new ParameterExpression[] { numParam });
+
+
+var isEven = IsEvenExpression.Compile();
+Console.WriteLine(isEven(10));
+Console.WriteLine(isEven(9));
